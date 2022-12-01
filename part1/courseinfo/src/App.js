1666877fa686d9d19dc67e6dca3cd7a55097ab04
Victor Kafka <vkafka@Mac-mini.home>
2022-12-01 13:59:52 +0100

    step 5

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,68 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+    </>
+  );
+};
+
+const Total = (props) => {
+  let cont = 0;
+
+  props.parts.map((part) => (cont += part.exercises));
+
+  return (
+    <>
+      <p>Number of exercises {cont}</p>
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,22 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const Hello = () => {
-  return (
-    <div>
-      <p>Hello World</p>
-    </div>
-  )
-}
-
-const App = () => {
-  return (
-    <div>
-     <h1>Greetings</h1>
-     <Hello />
-    </div>
-  )
-};
+import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
