a04db860c835ff19884f7331c968d0d03d1fe8df
Victor Kafka <vkafka@Mac-mini.home>
2022-12-07 14:45:57 +0100

    init anecdotes

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
