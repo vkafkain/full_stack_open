ddc633850182378771850f7ddc9f25e3f5c23f19
vkafkain <36845439+vkafkain@users.noreply.github.com>
2023-02-27 23:36:55 +0100

    controllers moved

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,7 +1,7 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog')
 
-blogRouter.get('/api/blogs', (request, response) => {
+blogRouter.get('/', (request, response) => {
     Blog
       .find({})
       .then(blogs => {
@@ -9,7 +9,7 @@ blogRouter.get('/api/blogs', (request, response) => {
       })
   })
   
-  app.post('/api/blogs', (request, response) => {
+  blogRouter.post('/', (request, response) => {
     const blog = new Blog(request.body)
   
     blog
