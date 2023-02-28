07e4f2f7bd27893a921b8e109f10d9cd2da9a3ab
Víctor López <vlopezmoles@gmail.com>
2023-02-28 12:52:00 +0100

    4.4 finish

--- part4/bloglist/utils/list_helpers.js
+++ part4/bloglist/utils/list_helpers.js
@@ -5,6 +5,18 @@ const dummy = (blogs) => {
 }
 
 
+const totalLikes = (blogs) => {
+  let total = 0
+  if(blogs.length === 0) {
+    return total
+  }
+  if(blogs.length > 0) {
+    return total += blogs.reduce((sum, post) => sum + post.likes, 0)
+  }
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
