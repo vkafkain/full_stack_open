ed14278cf3e13b5319a45eab0dc82b570d557d60
Víctor López <vlopezmoles@gmail.com>
2023-03-01 09:12:53 +0100

    update 4.5

--- part4/bloglist/test/favoriteBlog.test.js
+++ part4/bloglist/test/favoriteBlog.test.js
@@ -1,11 +1,6 @@
 const listHelper = require('../utils/list_helpers')
 
 describe('favorite blog', () => {
-  const favorite = [{
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    likes: 12
-  }]
 
   const listBlogs = 
 
