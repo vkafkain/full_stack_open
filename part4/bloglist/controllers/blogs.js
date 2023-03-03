e20eab20b068989033b81bed67a6952dcd483ce4
Víctor López <vlopezmoles@gmail.com>
2023-03-03 10:04:55 +0100

    finish 4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,8 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -10,11 +12,23 @@ blogsRouter.get('/', async (request, response) => {
     response.status(404).end()
   }
 })
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.toLowerCase().startsWith('beared')) {
+    return authorization.substring(7)
+  }
+  return null
+}
   
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  
-  const user = await User.findOne({})
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, config.SECRET)
+  if(!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid'})
+  }
+  const user = await User.findById(decodedToken.id)
 
   if(!body.title || !body.url){
     return response.status(400).end()
@@ -29,6 +43,9 @@ blogsRouter.post('/', async (request, response) => {
   })
  
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await blog.save()
+
   response.json(savedBlog)
 })
 
--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -5,9 +5,11 @@ let MONGODB_URI = process.env.MONGODB_URI
 if(process.env.NODE_ENV === 'test'){
   MONGODB_URI = process.env.TEST_MONGODB_URI
 }
+const SECRET = process.env.SECRET
 
 module.exports = {
   PORT,
-  MONGODB_URI
+  MONGODB_URI,
+  SECRET
 }
 
